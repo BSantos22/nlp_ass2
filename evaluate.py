a6a7c5a56d3e6116756ec6cd1a386a35be6454bb
David Rosenbusch <david.rosenbusch@googlemail.com>
2018-09-18 21:22:22 +0100

    clean up

--- evaluate.py
+++ evaluate.py
@@ -29,7 +29,9 @@ def read_predictions(filename):
 
 
 def normalize(text):
-    return text.lower().replace(unwanted, "")
+    for unwanted in unwantedTokens:
+        text = text.replace(unwanted, "")
+    return text.lower()
 
 
 ground_truth_file = sys.argv[1]
@@ -39,7 +41,7 @@ unwantedTokens = [',','.',':',';']
 minCount = 25
 
 tokenCount = {}
-popTokens = {}
+popularTokens = {}
 
 maxNGram = 3
 
@@ -47,27 +49,24 @@ totalNumWordsP = 0
 totalNumWordsN = 0
 totalNumWords = 0
 
-uniqueNGrams = [[set(),set()], [set(),set()], [set(),set()]]  # ngram
-bigramCount = {}
+uniqueNGrams = [[set(),set()], [set(),set()], [set(),set()]]    # array of n-grams for n = (1, 2, 3), split up to positive / negative
+bigramCount = {}                                                # count number of bigrams, split up to positive / negative
 totalNumBigramsP = 0
 totalNumBigramsN = 0
 totalNumBigrams = 0
 
-positiveProbabilities = {}
-negativeProbabilities = {}
-positiveProbabilitiesG = {}
-negativeProbabilitiesG = {}
-posReviews = [fi for fi in os.listdir("train/") if "P" in fi]
-negReviews = [fi for fi in os.listdir("train/") if "N" in fi]
+positiveProbabilities = {}                                      # dict key = token, value = probability
+negativeProbabilities = {}                                      # dict key = token, value = probability
+
+positiveProbabilitiesG = {}                                     # dict key = bigram, value = probability
+negativeProbabilitiesG = {}                                     # dict key = bigram, value = probability
 
 # Tokenizing (Task 4), count unique n-grams for n=1,2,3, count tokens
-for filename in os.listdir("train/"):
+for filename in os.listdir("train/"):                           # go through training set
     with open("train/" + filename, 'r') as myfile:
-        review = myfile.read()
-        for unwanted in unwantedTokens:
-            review = normalize(review)
+        review = normalize(myfile.read())                       # remove some unwanted letters & to lower case
         tokens = nltk.word_tokenize(review)
-        for tok in tokens:
+        for tok in tokens:                                      # count occurrence of all tokens for positive / negative reviews
             if tok not in tokenCount:
                 tokenCount[tok] = [0, 0]
             if "P" in filename:
@@ -75,7 +74,7 @@ for filename in os.listdir("train/"):
             else:
                 tokenCount[tok][1] += 1
 
-        for x in range(1, maxNGram+1):
+        for x in range(1, maxNGram+1):                          # count unique grams (1, 2, 3) and bigrams in positive / negative reviews
             grams = nltk.ngrams(tokens, x)
 
             for gram in grams:
@@ -84,61 +83,57 @@ for filename in os.listdir("train/"):
                         bigramCount[gram] = [0, 0]
 
                 if 'P' in filename:
-
                     if x == 2:
                         bigramCount[gram][0] += 1
                     uniqueNGrams[x - 1][0].add(gram)
+
                 if 'N' in filename:
                     if x == 2:
                         bigramCount[gram][1] += 1
                     uniqueNGrams[x - 1][1].add(gram)
 
-# Only keep tokens that occur at least 25 times (Task 4)
-for tc in tokenCount:
-    if tokenCount[tc][0]+tokenCount[tc][1] >= minCount:
-        popTokens[tc] = tokenCount[tc]
-sortedTokens = sorted(popTokens.items(), key=operator.itemgetter(1), reverse=True)
 
-print(bigramCount)
-for gramsPN in bigramCount:
+for tc in tokenCount:                                           # Only keep tokens that occur at least 25 times
+    if tokenCount[tc][0]+tokenCount[tc][1] >= minCount:
+        popularTokens[tc] = tokenCount[tc]
+sortedTokens = sorted(popularTokens.items(), key=operator.itemgetter(1), reverse=True)
+
+for gramsPN in bigramCount:                                     # count total number of bigrams for later calculations
     values = bigramCount[gramsPN]
     totalNumBigrams += values[0] + values[1]
     totalNumBigramsP += values[0]
     totalNumBigramsN += values[1]
 
-print(str(totalNumBigrams) + " -> " + str(totalNumBigramsP) + " + " + str(totalNumBigramsN))
-print(len(uniqueNGrams[1][0])+len(uniqueNGrams[1][1]))
-
-for tok in popTokens:
-    totalNumWordsP += popTokens[tok][0]
-    totalNumWordsN += popTokens[tok][1]
+for tok in popularTokens:                                       # count total number of tokens for later calculations
+    totalNumWordsP += popularTokens[tok][0]
+    totalNumWordsN += popularTokens[tok][1]
 
 totalNumWords = totalNumWordsP + totalNumWordsN
 
-for x in range(len(uniqueNGrams)):
-    print(str(x+1)+"-grams: " + str(len(uniqueNGrams[x][0])+len(uniqueNGrams[x][1])))
-print(sortedTokens)
 print("Top 10 words:")
 for x in range(10):
     print(sortedTokens[x])
-    
-'''
+
+'''                                                             # commented out because it is a very long list
 print("Rare words:")
 for tc in tokenCount:
     if tokenCount[tc] < 5:
         print(tc + ": " + str(tokenCount[tc]))
 '''
 
-k_smoothing = 0.01
+k_smoothing_task7 = 0.01  # best for task 7
+k_smoothing_task5 = 0.37  # best for task 5
+
 
 # Task 5
+# return the probability of a token given a positive or negative review
 def probabilityOfWGivenReviews(word, reviewType):
     if reviewType == 'P':
-        return (popTokens[word][0] + k_smoothing) / (totalNumWordsP + k_smoothing * totalNumWords)
-    return (popTokens[word][1] + k_smoothing) / (totalNumWordsN + k_smoothing * totalNumWords)
+        return (popularTokens[word][0] + k_smoothing_task5) / (totalNumWordsP + k_smoothing_task5 * totalNumWords)
+    return (popularTokens[word][1] + k_smoothing_task5) / (totalNumWordsN + k_smoothing_task5 * totalNumWords)
 
 
-p = []
+p = []                                                          # store the probabilities for all tokens (positive / negative)
 for token in sortedTokens:
     p = probabilityOfWGivenReviews(token[0], 'P')
     if p != 0:
@@ -150,14 +145,14 @@ for token in sortedTokens:
 
 
 # Task 7
+# return the probability of a bigram given a positive or negative review
 def probabilityOfWGivenReviewsG(g, reviewType):
-   # print("prob for " + str(g))
     if reviewType == 'P':
-        return (bigramCount[g][0] + k_smoothing) / (totalNumBigramsP + k_smoothing * totalNumBigrams)
-    return (bigramCount[g][1] + k_smoothing) / (totalNumBigramsN + k_smoothing * totalNumBigrams)
+        return (bigramCount[g][0] + k_smoothing_task7) / (totalNumBigramsP + k_smoothing_task7 * totalNumBigrams)
+    return (bigramCount[g][1] + k_smoothing_task7) / (totalNumBigramsN + k_smoothing_task7 * totalNumBigrams)
 
 
-p = []
+p = []                                                          # store the probabilities for all bigrams (positive / negative)
 for gr in bigramCount:
     pp = probabilityOfWGivenReviewsG(gr, 'P')
     if pp != 0:
@@ -167,14 +162,9 @@ for gr in bigramCount:
     if pn != 0:
         negativeProbabilitiesG[gr] = pn
 
-print("total #words p: "+str(totalNumWordsP))
-print("total #words n: "+str(totalNumWordsN))
-print("positive probs:")
-print(positiveProbabilities)
-print("negative probs:")
-print(negativeProbabilities)
 
-
+# classification of a review (tks being the token of the review) according to task 5 (using tokens)
+# returns an array of probabilities (positive / negative)
 def classifyTask5(tks):
     probs = [0.0, 0.0]  # p,n
     for to in tks:
@@ -185,6 +175,8 @@ def classifyTask5(tks):
     return probs
 
 
+# classification of a review (tks being the token of the review) according to task 7 (using bigrams)
+# returns an array of probabilities (positive / negative)
 def classifyTask7(tks):
     probs = [0.0, 0.0]  # p,n
     grms = nltk.ngrams(tks, 2)
@@ -204,14 +196,14 @@ def classifyTask7(tks):
 
 
 preds = []
-for filename in os.listdir("test/"):
+for filename in os.listdir("test/"):                        # predict for each test-review
     probs = [0.0, 0.0]  # p,n
     with open("test/" + filename, 'r') as myfile:
         reviewText = normalize(myfile.read())
         tks = nltk.word_tokenize(reviewText)
 
-        probs = classifyTask7(tks)
-        #probs = classifyTask5(tks)
+        #probs = classifyTask7(tks)
+        probs = classifyTask5(tks)
 
     if probs[0] < probs[1]:
         dclass = "N"
@@ -238,5 +230,5 @@ for ID in ground_truth_map:
 
 # Print summary
 acc = float(correct)/len(ground_truth_map)
-summary = "For k = " + str(k_smoothing) + ": " + str(correct) + " out of " + str(len(ground_truth_map)) + " were correct!\naccuracy " + str(acc)+"\n"
+summary = str(correct) + " out of " + str(len(ground_truth_map)) + " were correct!\naccuracy " + str(acc)+"\n"
 print(summary)
